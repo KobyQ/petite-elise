75e065d9d0fe4bee00b7d2a9976f3a8eecdc3f15
Phoebe-Emefa <phoebeemefa@gmail.com>
2025-07-30 13:42:58 +0000

    updates

--- src/app/(root)/saturday-kids-club/page.tsx
+++ src/app/(root)/saturday-kids-club/page.tsx
@@ -190,7 +190,7 @@ const SaturdayKidsClub = () => {
           const response = await fetch("https://api.paystack.co/transaction/initialize", {
             method: "POST",
             headers: {
-              Authorization: `Bearer ${process.env.NEXT_PUBLIC_PAYSTACK_SECRET_KEY}`,
+              Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
               "Content-Type": "application/json",
             },
             body: JSON.stringify({
